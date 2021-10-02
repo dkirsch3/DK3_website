587eb600e8971a72772d18ed54d36519ced7c52e
David <trey3@hotmail.com>
2021-10-01 22:16:34 -0500

    Images added

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function changeBackground(event) {
+    console.log(event.target);
+    event.target.style.background = "orange"
+}
 
+function backToNormal(event) {
+    event.target.style.background = ""
+}
